5af7fe91c0bafd2b3b9dc524c5bce9a1fd3ee2ac
OrzAngel <junche@seas.upenn.edu>
2016-11-18 15:08:53 -0500

    QuoteArray updated DONE

--- src/QuoteArray.java
+++ src/QuoteArray.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class QuoteArray {
@@ -16,17 +17,36 @@ public class QuoteArray {
 		if (!isQuote) {
 			if (isInQuote) {
 				quoteList.add(currentQuote);
+				isInQuote = false;
 			}
 			return;
 		}
 		if (!isInQuote) {
 			currentQuote = new Quote();
+			isInQuote = true;
 		}
 		currentQuote.addWord(word);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
+		String path = "files/metamorphosis.txt";
+		FileReader input = new FileReader(path);
+		Splitter sp = new Splitter();
+		QuoteArray qa = new QuoteArray();
+		
+		for(;;){
+			try {
+				path = sp.nextWord();
+				qa.addWord(path, sp.isInQuotation());
+			} catch (NeedNewLineException e) {
+				if (!input.hasNextLine()) {
+					break;
+				}
+				sp.readNextLine(input.nextLine());
+			}
+		}
+		System.out.println(path);
 
 	}
 
