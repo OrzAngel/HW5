709149b3cbba74fa5e563bb320c26f650b5aa60f
OrzAngel <junche@seas.upenn.edu>
2016-11-17 20:11:56 -0500

    link it to eclipse, hope this works

--- src/FileReader.java
+++ src/FileReader.java
@@ -5,23 +5,33 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 class FileReader {
-  private File input;
-  private Scanner in;
-  
-  public FileReader(String path) throws FileNotFoundException{
-    
-    input = new File(path);
-    in = new Scanner(input);
-    
-  }
-  
-  public boolean hasNextLine() {
-    return in.hasNextLine();
-  }
-  
-  public String nextLine() {
-    return in.nextLine();
-  }
-  
+	private File input;
+	private Scanner in;
+
+	public FileReader(String path) throws FileNotFoundException{
+
+		input = new File(path);
+		in = new Scanner(input);
+
+	}
+
+	public boolean hasNextLine() {
+		return in.hasNextLine();
+	}
+
+	public String nextLine() {
+		return in.nextLine();
+	}
+
+	public static void main(String[] args) {
+		String path = "files/alice-in-wonderland.txt";
+		try {
+			FileReader in = new FileReader(path);
+			System.out.println(in.nextLine());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
-    
+
